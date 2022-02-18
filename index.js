56d45ad88e0326e8ebf7b3b19fd8b9b813a5c9ee
Videogamer80 <kngainat@sfu.ca>
2022-02-17 19:04:23 -0800

    first commit i think

--- index.js
+++ index.js
@@ -2,9 +2,42 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+const { Pool } = require('pg');
+var pool = new Pool({
+    connectionString: "postgres://postgres:bootstrap@localhost/users"
+    // connectionString: "postgres://pdxjfyvkfttkpe:29b780b3e352b2f5e4b08595282cd64dc7af55c208966c27f6c9740f7551eab9@ec2-34-233-157-9.compute-1.amazonaws.com:5432/doucu0hcsvt85",
+    // ssl: {
+    //     rejectUnauthorized: false
+    // }
+})
+
+//stuff that needs to be included
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//what happens when you go to each url
+app.get('/', async (req, res) =>{
+  var UsersTable = `SELECT * FROM rect`;
+  pool.query(UsersTable, (error,result) => {
+    if (error) {
+      res.end(error);
+    }
+    var results = { 'rows' : result.rows };
+    res.render('pages/index', results);
+  })
+})
+
+app.get('/users/:id', (req,res)=>{
+  //per id stuff here
+})
+
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
